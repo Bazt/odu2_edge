c242e691a4e75cc2d035129abfbd9adb0707c48d
Oleg Shcherbakov <oleg.shcherbakov@viaccess-orca.com>
2021-12-21 18:02:48 +0200

    works ok for b)

--- odu2_edge/odu2_edge/main.cpp
+++ odu2_edge/odu2_edge/main.cpp
@@ -54,56 +54,65 @@ public:
         p.resize(n+1);
         q.resize(n+1);
         f.resize(n+1);
-        for (int i = 0; i <= n; i++) {
-            x[i] = (a + i * h);
-            p[i] = P(x[i]);
-            q[i] = Q(x[i]);
-            f[i] = F(x[i]);
-        }
         
         A.resize(n);
         B.resize(n);
         C.resize(n);
         FF.resize(n);
         
-        for (int i = 1; i <= n - 1; i++) {
-            A[i] = 1 / h / h - p[i] / 2 / h;
-            C[i] = 2 / h / h - q[i];
-            B[i] = 1 / h / h + p[i] / 2 / h;
-            FF[i] = 0;
+        for (int i = 0; i <= n; i++) {
+            x[i] = a + i * h;
+            A[i] = 1-P(x[i])*h/2;
+            B[i] = 1 + P(x[i])*h/2;
+            C[i] = 2-Q(x[i])*h*h;
+            FF[i] = h*h*F(x[i]);
         }
         
+    
+
         
-        cout << "Check |C| > |A| + |B|: ";
-        bool ok = true;
-        for (int i = 1; i <= n - 1; i++) {
-            if (abs(C[i]) > abs(A[i]) + abs(B[i])) {
-                ok = false;
-                break;
-            }
-        }
-        cout << ok << endl;
-        
+//        for (int i = 1; i <= n - 1; i++) {
+//            A[i] = 1 / h / h + p[i] / 2 / h;
+//            C[i] = -2 / h / h + q[i];
+//            B[i] = 1 / h / h - p[i] / 2 / h;
+//            FF[i] = 0;
+//        }
+//
+//
+//        cout << "Check |C| > |A| + |B|: ";
+//        bool ok = true;
+//        for (int i = 1; i <= n - 1; i++) {
+//            if (abs(C[i]) > abs(A[i]) + abs(B[i])) {
+//                ok = false;
+//                break;
+//            }
+//        }
+//        cout << ok << endl;
+//
         k.resize(n+1);
         v.resize(n+1);
         
         
+        double k1 = 1;
+        double k2 = 0;
+        double l1 = 1;
+        double l2 = 0;
+        
+        
+        
         // b)
-        k[0] = 0;
-        v[0] = y0;
         
-        k[n] = 0;
-        v[n] = yn;
-        
+        k[1] =-k2 / h*(k1-k2/h);
+        v[1] = y0 / (k1 - k2/h);
         for (int j = 1; j <= n - 1; j++) {
-            k[j] = -B[j] / (C[j] + A[j] * k[j-1]);
-            v[j] = - (A[j] * v[j - 1] - FF[j]) / (C[j] + A[j] * k[j-1]);
+            k[j+1] = B[j] / (C[j] - A[j] * k[j]);
+            v[j+1] = (A[j] * v[j] - FF[j]) / (C[j] - A[j] * k[j]);
         }
         
         yy.resize(n+1);
-        yy[n] = (v[n] + k[n]*v[n-1]) / (1 - k[n]*k[n-1]);
-        for (int j = n-1; j >=0; j--) {
-            yy[j] = k[j] * yy[j+1] + v[j];
+        yy[n] = (l2*v[n] + yn*h) / (l2+h*l1-l2*k[n]);
+        for (size_t j = n; j >= 1; j--) {
+            yy[j-1] = k[j] * yy[j] + v[j];
         }
         
         
@@ -116,11 +125,11 @@ int main(int argc, const char * argv[]) {
     solution s_b;
     s_b.a = 1e-3;
     s_b.b = 3;
-    s_b.n = 100;
+    s_b.n = 1000;
     s_b.y0 = s_b.u_touch(s_b.a);
     s_b.yn = s_b.u_touch(s_b.b);
     s_b.setup();
-    for (int i = 0; i <= 100; i++) {
+    for (int i = 0; i <= s_b.n; i++) {
         cout << s_b.x[i] << "\t\t" << s_b.yy[i] << endl;
     }
 
